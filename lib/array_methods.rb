98db48832c9c4fe60dca08d57d899f562c67d8ec
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2020-01-12 02:05:17 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -6,21 +6,26 @@ end
 def using_sort(array)
 array.sort 
 
-
 end
 
 def using_reverse(array)
+array.reverse
 
 end
 
 def using_first(array)
+array.first
+
 
 end
 
 def using_last(array)
+array.last
 
 end
 
 def using_size(array)
+
+array.length 
 
 end
